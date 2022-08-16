e6797c0701e0abc79e89beb5e9cef7055096615b
kadir polatkol <kpolatkol5@gmail.com>
2022-08-16 17:41:05 +0300

    daire alani ödevi

---
+++ src/daireAlani/DaireAlani.java
@@ -0,0 +1,24 @@
+package daireAlani;
+import java.util.Scanner;
+
+public class DaireAlani {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("dairenin capi : ");
+        int cap = input.nextInt();
+
+        System.out.print("aci degeri : ");
+        int aci = input.nextInt();
+
+        float pi = 3.14f;
+
+
+        System.out.println("dairenin alani : "+(pi * (cap*cap)) );
+
+        System.out.println("dairenin cevresi  : "+(pi*2*cap) );
+        System.out.println("istenilen acinin alani : " +(( pi * (cap*cap) * aci) /360))  ;
+
+    }
+}
